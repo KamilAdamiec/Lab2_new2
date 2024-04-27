ba09c813ae619c2674c12c640934ad7d460d745d
Kamil Adamiec <kamil.adamiec99@gmail.com>
2024-04-27 14:20:06 +0200

    Initial Commit

---
+++ Lab2_new2/Lab2_new2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2_new2App.swift
+//  Lab2_new2
+//
+//  Created by student on 2024-04-27.
+//
+
+import SwiftUI
+
+@main
+struct Lab2_new2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
